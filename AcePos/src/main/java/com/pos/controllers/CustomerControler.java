1d4afdd464b021c1e4da65f8a547c12102ce6c91
arifusman786 <arifusman@gmail.com>
2016-02-04 03:50:22 -0800

    Added handlers and service level support for retrieving list of employees.

--- AcePos/src/main/java/com/pos/controllers/CustomerControler.java
+++ AcePos/src/main/java/com/pos/controllers/CustomerControler.java
@@ -9,8 +9,11 @@ import com.pos.beans.customer.CustomerBean;
 import com.pos.model.customers.Customer;
 import com.pos.model.dtos.CustomerDto;
 import com.pos.model.persons.Person;
+import com.pos.service.CustomerService;
+import com.pos.service.PersonService;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -20,6 +23,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class CustomerControler {
+@Autowired
+    private PersonService personService;
+@Autowired
+    private CustomerService customerService;
 
    
 
@@ -47,13 +54,71 @@ public class CustomerControler {
     public String customerAdd(@ModelAttribute CustomerBean cbn) {
         return "customerAdd";
     }
+    
+    @RequestMapping(value = "/customerAdd", method = RequestMethod.POST)
+    public String customerAddPost(@ModelAttribute CustomerBean cbn) {
+        System.out.println("Going to add person");
+        
+        Customer cust = new Customer();
+        cust.setReferance(cbn.getReference());
+        cust.setTypeOfCustomer(cbn.getTypeOfCustomer());
+        
+        Person person = new Person();
+        person.setFirstName(cbn.getFirstName());
+        person.setLastName(cbn.getLastName());
+        person.setAge(cbn.getAge());
+        person.setEmail(cbn.getEmail());
+        person.setMobileNo(cbn.getMobilePhone());
+        person.setUserName(cbn.getUserName());
+        
+        person = personService.saveOrUpdatePerson(person);
+        cust.setPersonId(person.getPersonId());
+        System.out.println("Going to add customer for personId = "+person.getPersonId());
+        
+        customerService.saveOrUpdateCustomer(cust);
+        return "customerAdd";
+    }
 
     @RequestMapping(value = "customerList", method = RequestMethod.GET)
     public String customerList(Model model) {
         System.err.println("-----------customerList handler called"+customerList1());
         
-        model.addAttribute("customersList", customerList1());
+        //model.addAttribute("customersList", customerList1());
+        List<CustomerDto> listDtos = getCstomerDto();
+        System.out.println("+++++++++++++++++++++++++++++Size of customers = "+listDtos.size());
+        model.addAttribute("customersList", listDtos);
         return "customerList";
     }
- 
+    
+    
+    public List<CustomerDto> getCstomerDto(){
+        List<CustomerDto> listDtos = new ArrayList<>();
+        List<Customer> listCustomers = customerService.getAllCustomers();
+        Person person = null;
+        for(Customer cust : listCustomers){
+            person = personService.getPersonById(cust.getPersonId());
+            listDtos.add(new CustomerDto(person, cust));
+        }
+        
+        return listDtos;
+    }
+    
+    
+    
+    public void createCustomer(){
+        //customerService.getCustomerById(1);
+        Person person = new Person();
+        person.setFirstName("Michael");
+        person.setLastName("Zihestra");
+        System.err.println("----------------Going to create Person = "+person);
+        person = personService.saveOrUpdatePerson(person);
+        System.err.println("++++++++++++++++++++Newly Created Person = "+person);
+        Customer cust = new Customer();
+        cust.setPersonId(person.getPersonId());
+        cust.setReferance("My Referance");
+        cust.setTypeOfCustomer("Regular");
+        customerService.saveOrUpdateCustomer(cust);
+    }
+    
+    
 }
